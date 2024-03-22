02afd5b551d888695a377fa9f8bb1c754e1b8dc8
Arni <toarni@gmail.com>
2024-03-22 15:17:12 +0100

    feat: initial commit to have a playground

---
+++ src/main/java/org/labordigital/cryptomator_bitwarden/BitwardenAccess.java
@@ -0,0 +1,43 @@
+package org.labordigital.cryptomator_bitwarden;
+
+import org.cryptomator.integrations.keychain.KeychainAccessException;
+import org.cryptomator.integrations.keychain.KeychainAccessProvider;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.Nullable;
+
+public class BitwardenAccess implements KeychainAccessProvider {
+    @Override
+    public @Nls(capitalization = Nls.Capitalization.Title) String displayName() {
+        return null;
+    }
+
+    @Override
+    public void storePassphrase(String s, @Nullable String s1, CharSequence charSequence) throws KeychainAccessException {
+
+    }
+
+    @Override
+    public char[] loadPassphrase(String s) throws KeychainAccessException {
+        return new char[0];
+    }
+
+    @Override
+    public void deletePassphrase(String s) throws KeychainAccessException {
+
+    }
+
+    @Override
+    public void changePassphrase(String s, @Nullable String s1, CharSequence charSequence) throws KeychainAccessException {
+
+    }
+
+    @Override
+    public boolean isSupported() {
+        return false;
+    }
+
+    @Override
+    public boolean isLocked() {
+        return false;
+    }
+}
