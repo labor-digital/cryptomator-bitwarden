c446bdd6448d55446af730266fea109826092ec4
Arni <toarni@gmail.com>
2024-04-08 11:50:30 +0200

    fix: initial commit to have a playground

--- src/main/java/org/labordigital/cryptomator_bitwarden/Test.java
+++ src/main/java/org/labordigital/cryptomator_bitwarden/Test.java
@@ -1,13 +1,17 @@
 package org.labordigital.cryptomator_bitwarden;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class Test {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("TEST123456");
-        var process = Runtime.getRuntime()
-                .exec(String.format("cmd.exe bw unlock"));
+        //Process p = Runtime.getRuntime().exec("cmd /c start /wait bw.exe login --sso");
+        Process p = Runtime.getRuntime().exec("cmd /c start /wait bw.exe unlock");
+        OutputStream oStream = p.getOutputStream();
+        int exitCode = p.waitFor();
+        System.exit(exitCode);
     }
 
 }
